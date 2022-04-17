7947a9463735ce2599fb4aca5be12b013ed480f7
Dreezy <bankoleidris@gmail.com>
2022-04-17 12:22:08 +0000

    vite app/tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      MontserratBold: "Montserrat-Bold",
+      MontserratMedium: "Montserrat-Medium",
+      MontserratRegular: "Montserrat-Regular",
+      MontserratExtraBold: "Montserrat-ExtraBold",
+      MontserratLight: "Montserrat-Light",
+      MontserratExtraLight: "Montserrat-ExtraLight",
+    },
+    extend: {
+      colors: { regularWhite: "#ffffff", bgRed: "#D7263D" },
+      keyframes: {
+        shake: {
+          "10%, 90%": {
+            transform: " translate3d(-1px, 0, 0)",
+          },
+          "20%, 80%": {
+            transform: "translate3d(2px, 0, 0)",
+          },
+          "30%, 50%, 70%": {
+            transform: "translate3d(-4px, 0, 0)",
+          },
+          "40%, 60%": {
+            transform: "translate3d(4px, 0, 0)",
+          },
+        },
+      },
+      animation: {
+        shake: "shake 0.82s cubic-bezier(.36,.07,.19,.97) both",
+      },
+    },
+  },
+  plugins: [],
+};
