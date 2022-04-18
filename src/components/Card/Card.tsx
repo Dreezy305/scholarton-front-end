4be99b5141a70d653ae7d46344e576a827dfb638
Dreezy <bankoleidris@gmail.com>
2022-04-18 10:14:46 +0000

    our categories

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -29,9 +29,11 @@ const Card = ({ icon, text, type }: smallCardProps) =>
         {type === "CATEGORY" && (
           <div className="bg-white rounded-[30px] flex flex-col px-4 space-y-4 items-center text-center h-80 card-shadow relative">
             {icon}
-            <p className="absolute bottom-20">{text}</p>
+            <p className="absolute bottom-20 font-MontserratMedium font-medium text-base leading-7">
+              {text}
+            </p>
             <button
-              className="rounded-[10px] text-center px-3 py-2 items-center whitespace-nowrap absolute bottom-4 mb-4"
+              className="rounded-[10px] text-center px-3 py-2 items-center whitespace-nowrap absolute bottom-4 mb-4 text-white text-[18px]"
               style={{ background: "rgba(215, 38, 61, 0.6)" }}
             >
               Tell me more
