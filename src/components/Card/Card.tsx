cd169faf15421f895a14cc24eab0e757aa79013a
Dreezy <bankoleidris@gmail.com>
2022-04-18 09:17:48 +0000

    how we work

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -12,34 +12,33 @@ type categoryCardProps = {
   title?: string;
 };
 
-const Card = (
-  { icon, text, type }: smallCardProps,
-  { image, title }: categoryCardProps
-) => {
-  return (
-    <>
-      {type === "FLASH" && (
-        <div className="bg-white rounded-[20px] card-shado flex flex-col items-center h-32 w-32 card ">
-          {icon}
-          <p className="pt-3 font-MontserratMedium font-medium text-lg leading-7">
-            {text}
-          </p>
-        </div>
-      )}
+const Card = ({ icon, text, type }: smallCardProps) =>
+  // { image, title }: categoryCardProps
+  {
+    return (
+      <>
+        {type === "FLASH" && (
+          <div className="bg-white rounded-[20px] card-shado flex flex-col items-center h-32 w-32 card ">
+            {icon}
+            <p className="pt-3 font-MontserratMedium font-medium text-lg leading-7">
+              {text}
+            </p>
+          </div>
+        )}
 
-      {type === "CATEGORY" && (
-        <div className="bg-white rounded-[30px] flex flex-col">
-          {image}
-          <p>{title}</p>
-          <button
-            className="rounded-[10px] text-center px-4 py-2 items-center"
-            style={{ background: "rgba(215, 38, 61, 0.6)" }}
-          >
-            Tell me more
-          </button>
-        </div>
-      )}
-    </>
-  );
-};
+        {type === "CATEGORY" && (
+          <div className="bg-white rounded-[30px] flex flex-col px-4 space-y-4 items-center text-center h-80  mx-2">
+            {icon}
+            <p>{text}</p>
+            <button
+              className="rounded-[10px] text-center px-3 py-2 items-center whitespace-nowrap"
+              style={{ background: "rgba(215, 38, 61, 0.6)" }}
+            >
+              Tell me more
+            </button>
+          </div>
+        )}
+      </>
+    );
+  };
 export default Card;
--- src/screens/Categories/Categories.tsx
+++ src/screens/Categories/Categories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./categories.css";
 import Card from "../../components/Card/Card";
+import Maths from "../../assets/images/mathematics.png";
 
 const Categories = () => {
   return (
@@ -13,8 +14,14 @@ const Categories = () => {
           Build projects in line with your passion
         </p>
 
-        <div className="flex flex-row">
-          <Card type="CATEGORY" />
+        <div className="flex flex-row pb-5 mx-5 justify-evenly">
+          {[1, 2, 3, 4, 5, 6].map(() => (
+            <Card
+              type="CATEGORY"
+              icon={<img src={Maths} alt="Art_and_creativity" className="" />}
+              text=" Art and Creativity"
+            />
+          ))}
         </div>
       </div>
     </section>
