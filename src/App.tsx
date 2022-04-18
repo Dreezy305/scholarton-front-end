37acbfe4ea18b037cb3c1ccdf65898b576465ed7
Dreezy <bankoleidris@gmail.com>
2022-04-18 08:03:36 +0000

    how we work

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from "./screens/Navbar/Navbar";
 import Hero from "./screens/Hero/Hero";
 import About from "./screens/About/About";
 import How from "./screens/How-we-work/How";
+import Categories from "./screens/Categories/Categories";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <About />
       <How />
+      <Categories />
     </>
   );
 }
---
+++ src/screens/Categories/Categories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./categories.css";
+
+const Categories = () => {
+  return (
+    <section>
+      <div className=""></div>
+    </section>
+  );
+};
+
+export default Categories;
