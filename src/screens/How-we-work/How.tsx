5b4d2923bbf951853c2fd673706ebb24a3ea606a
Dreezy <bankoleidris@gmail.com>
2022-04-18 00:10:11 +0000

    mobile responsiveness

--- src/screens/How-we-work/How.tsx
+++ src/screens/How-we-work/How.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import Graduate2 from "../../assets/images/graduate2.png";
 
 const How = () => {
   return (
     <section id="how-we-work">
-      <div className="flex flex-row">
-        <h3 className="">How we work</h3>
+      <div className="flex flex-col items-center">
+        <h3 className="font-MontserratMedium font-semibold text-4xl leading-10 mb-10">
+          How we work
+        </h3>
+        {/* STEPS */}
+        <div className="flex flex-row">
+          <div className="border border-skyBlue rounded-full flex items-center text-center px-6 py-6">
+            <img src={Graduate2} alt="grad_2" className="" />
+          </div>
+
+          <div className="border border-skyBlue rounded-full flex items-center text-center px-6 py-6">
+            <img src={Graduate2} alt="grad_2" className="" />
+          </div>
+
+          <div className="border border-skyBlue rounded-full flex items-center text-center px-6 py-6">
+            <img src={Graduate2} alt="grad_2" className="" />
+          </div>
+        </div>
       </div>
     </section>
   );
