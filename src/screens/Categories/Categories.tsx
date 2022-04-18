479395497d6689757cf6e64743ba2093a1b797d7
Dreezy <bankoleidris@gmail.com>
2022-04-18 10:04:56 +0000

    our categories

--- src/screens/Categories/Categories.tsx
+++ src/screens/Categories/Categories.tsx
@@ -23,7 +23,7 @@ const Categories = () => {
 
   return (
     <section id="categories" className="pb-8">
-      <div className="container mx-auto bg-lightBg rounded-[100px] h-auto w-auto px-8 py-8 border">
+      <div className="container mx-auto bg-lightBg rounded-[100px] h-auto w-auto px-8 py-8 border sides">
         <h4 className="text-title font-MontserratMedium font-semibold lg:text-3xl md:text-2xl text-xl leading-10 text-center pt-5">
           Our Categories
         </h4>
