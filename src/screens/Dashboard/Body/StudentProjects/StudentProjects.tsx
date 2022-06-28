0796b8c63236dade4f4ec89ef843245ff74ca92f
Dreezy <bankoleidris@gmail.com>
2022-06-28 22:35:07 +0000

    student projects page

--- src/screens/Dashboard/Body/StudentProjects/StudentProjects.tsx
+++ src/screens/Dashboard/Body/StudentProjects/StudentProjects.tsx
@@ -5,7 +5,11 @@ import "./studentproject.css";
 const StudentProjects = () => {
   return (
     <Dashboard title="Project">
-      <div className="container mx-auto"></div>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-4">
+          <div className="bg-pink "></div>
+        </div>
+      </div>
     </Dashboard>
   );
 };
